4d2ed135c57d09826ae840f2612c823f7fbadccb
shmily <go4thx@gmail.com>
2016-09-02 20:35:27 +0900

    aws ip

--- src/main/java/jp/co/ananasgroup/Application.java
+++ src/main/java/jp/co/ananasgroup/Application.java
@@ -27,7 +27,7 @@ public class Application implements CommandLineRunner {
     return new WebMvcConfigurerAdapter() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedOrigins("http://localhost:3001");
+        registry.addMapping("/api/**").allowedOrigins("http://54.249.3.108:3001");
       }
     };
   }
