821a4c793716e5f53d19e92893982b02b859aa93
shmily <go4thx@gmail.com>
2016-09-02 20:33:54 +0900

    localhost

--- src/main/java/jp/co/ananasgroup/Application.java
+++ src/main/java/jp/co/ananasgroup/Application.java
@@ -3,6 +3,7 @@ package jp.co.ananasgroup;
 import jp.co.ananasgroup.entity.Product;
 import jp.co.ananasgroup.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -26,7 +27,7 @@ public class Application implements CommandLineRunner {
     return new WebMvcConfigurerAdapter() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedOrigins("http://localhost:3001");
+        registry.addMapping("/api/**").allowedOrigins("http://192.168.3.3:3001");
       }
     };
   }
